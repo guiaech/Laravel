4d000106343b56ee00653b7093f9b5d3181b8477
Guilherme Andrade <guilherme@live.it>
2022-03-07 17:08:09 -0300

    Criação pag criar eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,31 @@
 
 @section('content')
 
- <h1>Create event</h1>
+<div id="event-creat-container" class="col-md-6 offset-md-3">
+    <h1>Crie seu evento</h1>
+    <form action="/events" methods="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+        </div>
+        <div class="form-group">
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="pivate" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar evento">
+    </form>
+</div>
 
 @endsection
